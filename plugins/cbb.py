46aad83f0701db290af08df69330d0fadb0ddd6b
shinubo28always <shinubo28always@gmail.com>
2025-11-03 17:12:09 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import Client 
 from bot import Bot
 from config import *
@@ -41,9 +32,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         await query.message.edit_text(
             text=START_MSG.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
-                 InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')]
+            reply_markup = InlineKeyboardMarkup([
+                [InlineKeyboardButton('Aɴɪᴍᴇs', url='https://t.me/Hindi_Dub_Anime_Zone'),
+                InlineKeyboardButton('Bᴀsᴇ', url='https://t.me/AniReal_Anime_Zone')],
+                [InlineKeyboardButton('• ᴀʙᴏᴜᴛ', callback_data='about'),
+                InlineKeyboardButton(' ʜᴇʟᴘ •', callback_data='help')],
+                [InlineKeyboardButton("Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url='https://t.me/AniReal_Updates')]
             ])
         )
 
